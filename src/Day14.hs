11ac361dd514d50b81e630b778e49864fe0e862a
Dustin Sallings <dustin@spy.net>
2019-12-29 11:57:47 -0800

    Day 14 part 2

--- src/Day14.hs
+++ src/Day14.hs
@@ -1,6 +1,8 @@
 module Day14 where
 
 import           Advent.AoC                 (Parser, parseFile)
+import           Data.Map.Strict            (Map)
+import qualified Data.Map.Strict            as Map
 import           Data.Text                  (Text)
 import qualified Data.Text                  as T
 import           Text.Megaparsec            (endBy, many)
@@ -27,15 +29,27 @@ getInput = parseFile parseAll
     parseDuration = lexeme L.decimal <* lexeme "seconds, but then must rest for "
     parseRest = lexeme L.decimal <* "seconds."
 
-deerDistance :: Int -> Deer -> Int
-deerDistance n Deer{..} = min duration b * speed + a * dist
+distance :: Int -> Deer -> Int
+distance n Deer{..} = min duration b * speed + a * dist
   where
     (a, b) = n `divMod` unit
     dist = speed * duration
     unit = duration + rest
 
+score :: [Deer] -> Int -> Map Text Int
+score deer rounds = go mempty rounds
+  where
+    go m 0 = m
+    go m n = go m' (n - 1)
+      where
+        speeds = [(distance n d, d) | d <- deer]
+        winners = [d | (dist,d) <- speeds, dist == (maximum $ fst <$> speeds)]
+        m' = Map.unionWith (+) (Map.fromList [(name winner, 1) | winner <- winners]) m
+
 part1 :: IO Int
-part1 = maximum . map (deerDistance 2503) <$> getInput "input/day14"
+part1 = maximum . map (distance 2503) <$> getInput "input/day14"
 
 part2 :: IO Int
-part2 = pure 0
+part2 = do
+  deer <- getInput "input/day14"
+  pure $ maximum . Map.elems . score deer $ 2503

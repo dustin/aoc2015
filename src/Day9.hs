e339ac330685aeb65e281f085b304a3c4f47ab55
Dustin Sallings <dustin@spy.net>
2019-12-30 14:18:03 -0800

    Don't need fancy pathfinding for Day 9.
    But I can use fancy monoids.

--- src/Day9.hs
+++ src/Day9.hs
@@ -1,11 +1,10 @@
 module Day9 where
 
 import           Advent.AoC                 (Parser, parseFile)
-import           Advent.Search              (bfs, dijkstra)
-import           Data.List.Extra            (maximumOn)
+import           Data.List                  (permutations)
 import           Data.Map.Strict            (Map)
 import qualified Data.Map.Strict            as Map
-import           Data.Set                   (Set)
+import           Data.Semigroup             (Max (..), Min (..))
 import qualified Data.Set                   as Set
 import           Data.Text                  (Text)
 import qualified Data.Text                  as T
@@ -18,60 +17,24 @@ data Route = Route !Text !Text !Int deriving Show
 lexeme :: Parser a -> Parser a
 lexeme = L.lexeme space
 
-getInput :: FilePath -> IO [Route]
-getInput fp = parseFile parseAll fp
+type Costs = Map (Text,Text) Int
+
+getInput :: FilePath -> IO Costs
+getInput fp = costs <$> parseFile parseAll fp
   where parseAll = parseRoute `endBy` "\n"
         parseRoute = Route <$> lexeme loc <*> (lexeme "to" *> lexeme loc) <*> (lexeme "=" *> L.decimal)
         loc = T.pack <$> many letterChar
 
-getInput' :: FilePath -> IO [Route]
-getInput' fp = map r . lines <$> readFile fp
-  where r x = case words x of
-                [a, "to", b, "=", c] -> Route (T.pack a) (T.pack b) (read c)
-                _                    -> error "bad line"
+        costs = Map.fromList . foldMap (\(Route f t c) -> [((t,f),c),((f,t),c)])
 
-mkMap :: [Route] -> Map Text [(Int, Text)]
-mkMap = Map.fromListWith (<>) . foldMap (\(Route f t c) -> [(t,[(c,f)]), (f,[(c,t)])])
-
-allDests :: [Route] -> Set Text
-allDests = Set.fromList . map (\(Route _ t _) -> t)
-
-data RState = RState {
-  curLoc :: Text,
-  seen   :: Set Text
-  } deriving (Show, Ord, Eq)
-
-findPath :: [Route] -> Maybe (Int, [Text])
-findPath rs = (fmap . fmap . fmap) curLoc $ dijkstra nf (RState "" mempty) ((== everywhere) . seen)
+most :: Monoid m => (Int -> m) -> Costs -> m
+most f m = foldMap (f . cost) . permutations $ allDests
   where
-    m = mkMap rs
-    everywhere = Map.keysSet m
-    nf RState{curLoc=""} = [(0,RState k (Set.singleton k)) | k <- Map.keys m]
-    nf r@RState{..} = [(c, r{curLoc=k, seen=Set.insert k seen})
-                      | (c,k) <- Map.findWithDefault [] curLoc m,
-                        k `Set.notMember` seen]
+    cost p = sum . zipWith (curry (m Map.!)) p $ tail p
+    allDests = Set.toList . Set.fromList . map fst . Map.keys $ m
 
-data RState2 = RState2 {
-  curLoc2 :: Text,
-  seen2   :: Set Text,
-  cost    :: Int
-  } deriving (Show, Ord, Eq)
+part1 :: Costs -> Int
+part1 = getMin . most Min
 
-bfsPath :: [Route] -> [RState2]
-bfsPath rs = filter ((== everywhere) . seen2) $ bfs nf (RState2 "" mempty 0)
-  where
-    m = mkMap rs
-    everywhere = Map.keysSet m
-    nf RState2{curLoc2=""} = [RState2 k (Set.singleton k) 0 | k <- Map.keys m]
-    nf r@RState2{..} = [r{curLoc2=k, seen2=Set.insert k seen2, cost=cost+c}
-                       | (c,k) <- Map.findWithDefault [] curLoc2 m,
-                         k `Set.notMember` seen2]
-
-part1 :: IO (Maybe Int)
-part1 = fmap fst . findPath <$> getInput "input/day9"
-
-part2' :: [Route] -> Int
-part2' = cost . maximumOn cost . bfsPath
-
-part2 :: IO Int
-part2 = part2' <$> getInput "input/day9"
+part2 :: Costs -> Int
+part2 = getMax . most Max
--- test/Day9Tests.hs
+++ test/Day9Tests.hs
@@ -1,5 +1,6 @@
 module Day9Tests where
 
+import           Data.Semigroup        (Min (..))
 import           Test.QuickCheck
 import           Test.Tasty
 import           Test.Tasty.HUnit
@@ -8,16 +9,16 @@ import           Test.Tasty.QuickCheck as QC
 import           Day9
 
 testEx1 :: Assertion
-testEx1 = assertEqual "" (Just 605) =<< (fmap fst . findPath <$> getInput "input/day9.example")
+testEx1 = assertEqual "" 605 =<< (part1 <$> getInput "input/day9.example")
 
 testPart1 :: Assertion
-testPart1 = assertEqual "" (Just 207) =<< part1
+testPart1 = assertEqual "" 207 =<< (part1 <$> getInput "input/day9")
 
 testEx2 :: Assertion
-testEx2 = assertEqual "" 982 =<< (part2' <$> getInput "input/day9.example")
+testEx2 = assertEqual "" 982 =<< (part2 <$> getInput "input/day9.example")
 
 testPart2 :: Assertion
-testPart2 = assertEqual "" 804 =<< part2
+testPart2 = assertEqual "" 804 =<< (part2 <$> getInput "input/day9")
 
 
 tests :: [TestTree]
